26ab26d64443de4964fff79fed00a59cea279798
Hyungeol94 <ekfrhkquf12@naver.com>
2025-12-20 15:07:03 +0900

    실습 수행

--- webpack.config.js
+++ webpack.config.js
@@ -8,5 +8,24 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve('./dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      },
+      {
+        test: /\.(jpg|png)$/, 
+        loader: 'url-loader',
+        options: {
+          name: '[name].[ext]?[hash]',
+          publicPath: './dist',
+          limit: 10000 //10kb 미만은 url로 처리, 그 이상은 file-loader가 처리
+        }
+      }
+    ]
   }
 }
